3475da96487172292a7d2c4244dfb877e107fe4b
Andrew Dibble <andrewdibble14@outlook.com>
2022-05-03 16:35:23 -0700

    added priestess class

---
+++ DungeonAndMonsters/Priestess.java
@@ -0,0 +1,40 @@
+public class Priestess extends Hero{
+
+    protected Priestess(final String theName) {
+
+        super(theName, 75, 5, 45, 25, .7, .3);
+    }
+
+
+    /**
+     * Special: Healing. For every point of damage dealt in next attack, priestess will heal 2/3 the amount.
+     * @param theChar object to deal damage to
+     */
+    @Override
+    protected void special(DungeonCharacter theChar) {
+        double randAccuracy = MY_RAND.nextDouble();
+        int specialMaxDamage = 45;
+        int damage = MY_RAND.nextInt(specialMaxDamage) + this.getMaxDamage();
+
+        System.out.println(this.getName() + " goes for a special healing attack!\n\n");
+
+        double specialAccuracy = .5;
+        if (specialAccuracy < randAccuracy) {
+            System.out.println("The healing attack misses!");
+        } else if (specialAccuracy > randAccuracy) {
+
+            int healing = (int) Math.ceil(damage*.66);
+
+            System.out.println(this.getName() + " lands the attack! Dealt " + damage + " of damage and healed " + healing+"points of health\n");
+
+            this.setHealth(this.getHealth() + healing);
+            int result = theChar.getHealth() - damage;
+            if (result < 0) {
+                result = 0;
+            }
+            theChar.setHealth(result);
+        }
+
+
+    }
+}
