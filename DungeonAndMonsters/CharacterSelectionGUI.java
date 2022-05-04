8d73958964f58d657aacfec6bfacbc8f11cf1ac5
Alex <humphriesa02@gmail.com>
2022-05-03 19:41:23 -0700

    Character Select GUI changes

--- DungeonAndMonsters/CharacterSelectionGUI.java
+++ DungeonAndMonsters/CharacterSelectionGUI.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class CharacterSelectionGUI extends JPanel{
-
+        Font customFont;
     CharacterSelectionGUI(){
+
         try {
             //create the font to use. Specify the size!
-            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("DungeonAndMonsters/VT323-Regular.ttf")).deriveFont(12f);
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("DungeonAndMonsters/VT323-Regular.ttf")).deriveFont(12f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             //register the font
             ge.registerFont(customFont);
@@ -16,20 +17,16 @@ public class CharacterSelectionGUI extends JPanel{
             e.printStackTrace();
         }
 
-        // Settings for our internal frame
+        // Settings for our internal panel
         // bg color and how it is laid out.
+        // as well as the grid bag constraints
+        GridBagConstraints gbc = new GridBagConstraints();
         this.setBackground(new Color(32,42,68));
         this.setLayout(new GridBagLayout());
 
-        // Defining a panel for our characters
-        // as well as the grid bag constraints
-        JPanel characterPanel = new JPanel();
-        GridBagConstraints gbc = new GridBagConstraints();
-        characterPanel.setBackground(new Color(32,42,68));
-        characterPanel.setLayout(new GridBagLayout());
-
         // settings for our initial images
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.insets = new Insets(0, 5, 0, 5);
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
         // Image placement ----------------------------------------
@@ -37,67 +34,32 @@ public class CharacterSelectionGUI extends JPanel{
         gbc.gridx = 0;
         gbc.gridy = 0;
         JLabel label = new JLabel(new ImageIcon("DungeonAndMonsters/archer.jpeg"));
-        characterPanel.add(label, gbc);
+        this.add(label, gbc);
 
         //Mage
         gbc.gridx = 1;
         gbc.gridy = 0;
         JLabel label2 = new JLabel(new ImageIcon("DungeonAndMonsters/mage.jpeg"));
-        characterPanel.add(label2, gbc);
+        this.add(label2, gbc);
 
         //Thief
         gbc.gridx = 2;
         gbc.gridy = 0;
         JLabel label3 = new JLabel(new ImageIcon("DungeonAndMonsters/thief.jpeg"));
-        characterPanel.add(label3, gbc);
+        this.add(label3, gbc);
 
         //Warrior
         gbc.gridx = 3;
         gbc.gridy = 0;
         JLabel label4 = new JLabel(new ImageIcon("DungeonAndMonsters/warrior.jpeg"));
-        characterPanel.add(label4, gbc);
+        this.add(label4, gbc);
 
         //Priestess
         gbc.gridx = 4;
         gbc.gridy = 0;
         JLabel label5 = new JLabel(new ImageIcon("DungeonAndMonsters/priestess.jpeg"));
-        characterPanel.add(label5, gbc);
+        this.add(label5, gbc);
         // image placement end ----------------------------------------------------------------
-
-        // button placement start -------------------------------------------------------------
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.ipady = 20;
-        //Archer
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        JButton archerButton = new JButton("Archer");
-        characterPanel.add(archerButton, gbc);
-
-        //Mage
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        JButton mageButton = new JButton("Mage");
-        characterPanel.add(mageButton, gbc);
-
-        //Thief
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        JButton thiefButton = new JButton("Thief");
-        characterPanel.add(thiefButton, gbc);
-
-        //Warrior
-        gbc.gridx = 3;
-        gbc.gridy = 1;
-        JButton warriorButton = new JButton("Warrior");
-        characterPanel.add(warriorButton, gbc);
-
-        //Priestess
-        gbc.gridx = 4;
-        gbc.gridy = 1;
-        JButton priestessButton = new JButton("Priestess");
-        characterPanel.add(priestessButton, gbc);
-
-        // button placement end -----------------------------------------------
 
         // Character description texts start ----------------------------------
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -105,65 +67,108 @@ public class CharacterSelectionGUI extends JPanel{
 
         //Archer
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         String archerFormat = DungeonAdventure.heroInfo("Archer").toString().replace("\n", "<br>");
-        String finalArcherFormat = "<html><font size='3'>" + archerFormat + "</font></html>";
+        String finalArcherFormat = "<html><style>" +
+                "h1 {text-align: center;}\n" +
+                "</style><h1><<font size='5'>" + archerFormat + "</h1></font></html>";
         JLabel archerDesc = new JLabel(finalArcherFormat);
+        archerDesc.setFont(customFont);
         archerDesc.setForeground(Color.white);
-        characterPanel.add(archerDesc, gbc);
+        this.add(archerDesc, gbc);
 
         //Mage
         gbc.gridx = 1;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         String mageFormat = DungeonAdventure.heroInfo("Mage").toString().replace("\n", "<br>");
         String finalMageFormat = "<html><font size='3'>" + mageFormat + "</font></html>";
         JLabel mageDesc = new JLabel(finalMageFormat);
         mageDesc.setForeground(Color.white);
-        characterPanel.add(mageDesc, gbc);
+        this.add(mageDesc, gbc);
 
         //Thief
         gbc.gridx = 2;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         String thiefFormat = DungeonAdventure.heroInfo("Thief").toString().replace("\n", "<br>");
         String finalThiefFormat = "<html><font size='3'>" + thiefFormat + "</font></html>";
         JLabel thiefDesc = new JLabel(finalThiefFormat);
         thiefDesc.setForeground(Color.white);
-        characterPanel.add(thiefDesc, gbc);
+        this.add(thiefDesc, gbc);
 
         //Warrior
         gbc.gridx = 3;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         String warriorFormat = DungeonAdventure.heroInfo("Warrior").toString().replace("\n", "<br>");
         String finalWarriorFormat = "<html><font size='3'>" + warriorFormat + "</font></html>";
         JLabel warriorDesc = new JLabel(finalWarriorFormat);
         warriorDesc.setForeground(Color.white);
-        characterPanel.add(warriorDesc, gbc);
+        this.add(warriorDesc, gbc);
 
         //Priestess
         gbc.gridx = 4;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         String priestessFormat = DungeonAdventure.heroInfo("Priestess").toString().replace("\n", "<br>");
         String finalPriestessFormat = "<html><font size='3'>" + priestessFormat + "</font></html>";
         JLabel priestessDesc = new JLabel(finalPriestessFormat);
         priestessDesc.setForeground(Color.white);
-        characterPanel.add(priestessDesc, gbc);
+        this.add(priestessDesc, gbc);
         // Character description texts end ------------------------------------------
 
-        // final settings for placing the character panel in our frame
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.weighty = 1;
-        this.add(characterPanel, gbc);
 
+        // button placement start -------------------------------------------------------------
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.ipady = 20;
+        //Archer
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        JButton archerButton = new JButton("Archer");
+        this.add(archerButton, gbc);
+
+        //Mage
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        JButton mageButton = new JButton("Mage");
+        this.add(mageButton, gbc);
+
+        //Thief
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        JButton thiefButton = new JButton("Thief");
+        this.add(thiefButton, gbc);
+
+        //Warrior
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        JButton warriorButton = new JButton("Warrior");
+        this.add(warriorButton, gbc);
+
+        //Priestess
+        gbc.gridx = 4;
+        gbc.gridy = 2;
+        JButton priestessButton = new JButton("Priestess");
+        this.add(priestessButton, gbc);
+
+        // button placement end -----------------------------------------------
+
+
+        gbc.anchor = GridBagConstraints.SOUTH;
+        gbc.gridx = 1;
+        gbc.gridy = 3;
         //TODO make these buttons appear at the bottom of the screen
         JButton mainMenu = new JButton("MAIN MENU");
         mainMenu.setFont(new Font("Serif", Font.BOLD, 20));
         mainMenu.setPreferredSize(new Dimension(200,50));
         mainMenu.addActionListener(e ->  DungeonAdventure.sceneController("menu"));
-        //characterPanel.add(mainMenu);
+        this.add(mainMenu, gbc);
 
+        gbc.gridx = 3;
+        gbc.gridy = 3;
         JButton monsters = new JButton("MONSTERS");
         monsters.setFont(new Font("Serif", Font.BOLD, 20));
         monsters.setPreferredSize(new Dimension(200,50));
         //monsters.addActionListener();
+        this.add(monsters,gbc);
+
     }
 }
