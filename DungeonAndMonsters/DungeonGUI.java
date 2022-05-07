c4e407ba742eff4bba4ecb53f1a4a8ea30e5ae4a
Alex <humphriesa02@gmail.com>
2022-05-07 00:55:04 -0700

    GungeonGUI and DungeonAdventure changes!

--- DungeonAndMonsters/DungeonGUI.java
+++ DungeonAndMonsters/DungeonGUI.java
@@ -1,17 +1,30 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class DungeonGUI extends JPanel {
     // white outline border to be used in dungeon
     private static final Border OUTLINE_BORDER = BorderFactory.createLineBorder(Color.WHITE, 5);
 
+    // Hero reference
+    private static Hero myDungeonHero;
+
+    // Health indicator reference
+    private static JLabel myHealthLabel;
+
+    // Room indicator (we are in room 0,0)
+    private static JLabel myRoomLabel;
+
+    // Image for the hero above health
+    private static JLabel myHeroImage;
+
+    // Name for hero
+    private static JLabel myHeroName;
+
+    // TODO |ANY VALUE THAT WILL CHANGE ON THIS PAGE I.E. HEALTH SHOULD HAVE A STATIC LABEL
+    // TODO |OR TEXT OR WHATEVER THAT WE CHANGE OUTSIDE
     DungeonGUI(Font thePixelFont) {
-        Hero hero = DungeonAdventure.getMyHero();
+        //Hero hero = DungeonAdventure.getMyHero();
         // regular layout setup
         GridBagConstraints gbc = new GridBagConstraints();
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
@@ -24,55 +37,117 @@ public class DungeonGUI extends JPanel {
         displayPanel.setBorder(OUTLINE_BORDER);
         this.add(displayPanel, gbc);
 
+        // Room indicator
+        myRoomLabel = new JLabel("Room: [0,0]");
+        myRoomLabel.setForeground(Color.white);
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0,0,0,20);
+        displayPanel.add(myRoomLabel,gbc);
+
+        gbc.gridx = 1;
         gbc.gridy = 0;
         JLabel dungeonLabel = new JLabel(new ImageIcon("DungeonAndMonsters/dungeon pics/Dungeon_1_down.png"));
         displayPanel.add(dungeonLabel, gbc);
 
 
+
+        // TODO Will split up button panel into to areas
         //--------------------------------buttons
         JPanel buttonPanel = new JPanel(new GridBagLayout());
         buttonPanel.setOpaque(true);
         buttonPanel.setBackground(Color.BLACK);
         buttonPanel.setBorder(OUTLINE_BORDER);
         this.add(buttonPanel, gbc);
-
-        gbc.anchor = GridBagConstraints.SOUTH;
-        gbc.insets = new Insets(10, 5, 0, 5);
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weighty = 1;
+
+        //This area is for player image, name and health
+        JPanel playerArea = new JPanel(new GridBagLayout());
+        playerArea.setOpaque(false);
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.weightx = 1;
+        gbc.weighty = 1;
+        buttonPanel.add(playerArea);
+
+        JPanel buttonArea = new JPanel(new GridBagLayout());
+        buttonArea.setOpaque(false);
+        gbc.anchor = GridBagConstraints.EAST;
+        buttonPanel.add(buttonArea);
+        //gbc.weighty = 1;
+
+        // Player portrait
         gbc.gridx = 0;
         gbc.gridy = 1;
+        myHeroImage = new JLabel(new ImageIcon("DungeonAndMonsters/character pics/archerpixel.png"));
+        myHeroImage.setBorder(OUTLINE_BORDER);
+        playerArea.add(myHeroImage, gbc);
 
+        // Player health text
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        myHealthLabel = new JLabel();
+        myHealthLabel.setFont(thePixelFont);
+        myHealthLabel.setForeground(Color.WHITE);
+        playerArea.add(myHealthLabel, gbc);
+
+
+        //end buttons --------------------------------------------
+        gbc.insets = new Insets(5,5,5,5);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        myHeroName = new JLabel();
+        myHeroName.setFont(thePixelFont);
+        myHeroName.setForeground(Color.WHITE);
+        buttonArea.add(myHeroName, gbc);
+
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
         JButton backpack = new JButton("Backpack");
         backpack.setFont(thePixelFont);
-        buttonPanel.add(backpack, gbc);
+        buttonArea.add(backpack, gbc);
         backpack.addActionListener(e -> {
             DungeonAdventure.sceneController("backpack");
         });
 
         gbc.gridx = 1;
-        gbc.gridy = 1;
+        gbc.gridy = 2;
         JButton map = new JButton("Map");
         map.setFont(thePixelFont);
-        buttonPanel.add(map, gbc);
+        buttonArea.add(map, gbc);
         map.addActionListener(e -> {
             DungeonAdventure.sceneController("map");
         });
-        //end buttons --------------------------------------------
 
-        //TODO label not showing up
-        try{
-            JLabel healthLabel = new JLabel("Current Health: " );//+ hero.getHealth());
-            healthLabel.setFont(new Font("Serif", Font.BOLD, 20));
-            healthLabel.setForeground(Color.WHITE);
-            healthLabel.setBackground(Color.BLACK);
-            this.add(healthLabel, gbc);
-        }catch (NullPointerException n){
-            System.out.println("hero not initialized");
-        }
+
+
 
     }
 
+    public static void setUpVisualDungeon(Hero theHero, Dungeon theDungeon){
+        setDungeonHero(theHero);
+        setMyHeroImage(theHero);
+        setMyHeroName(theHero);
+        //setMyHeroImage(theHero);
+        //setMyRoomLabel(theDungeon);
+    }
+
+    private static void setDungeonHero(Hero theHero){
+        myDungeonHero = theHero;
+        myHealthLabel.setText("Current health: " + myDungeonHero.getHealth());
+    }
+
+    private static void setMyHeroImage(Hero theHero){
+        //myHeroImage.setIcon(theHero.getImage());
+    }
+
+    private static void setMyHeroName(Hero theHero){
+        myHeroName.setText(theHero.getName());
+    }
+
+    private static void setMyRoomLabel(Dungeon theDungeon){
+        myRoomLabel.setText("Current room: " + theDungeon.getCurrentRoom().getXCoord()
+                + ", " + theDungeon.getCurrentRoom().getYCoord());
+    }
 }
